b88edcff38b94775f113df4987659da5fe951bec
Dev Adarsh <devadarsh4900@gmail.com>
2021-06-23 01:37:01 +0530

    Update 1281. Subtract the Product and Sum of Digits of an Integer.java

--- 1281. Subtract the Product and Sum of Digits of an Integer.java
+++ 1281. Subtract the Product and Sum of Digits of an Integer.java
@@ -1,7 +1,7 @@
 public class Subtract the Product and Sum of Digits of an Integer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 int n=234;
 int s=0,p=1,n1=1,r=0;
 while(n>0)
