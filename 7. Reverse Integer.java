d0173847c8636ed691b2a1c80a0f90a1d0d475dd
Dev Adarsh <devadarsh4900@gmail.com>
2021-06-26 21:37:51 +0530

    Update 7. Reverse Integer.java

--- 7. Reverse Integer.java
+++ 7. Reverse Integer.java
@@ -1,14 +1,14 @@
-public class Reverse Integer {
-
-	public static void main(String[] args) {
-		int x=1534236469;
-		long rev = 0;
+class Solution {
+    public int reverse(int x) {
+         long rev = 0;
         while(x != 0) {
             rev = rev * 10 + (x % 10);
             x /= 10;
         }
         if(rev > 2147483647||rev < -2147483647) {
             rev=0;
-        }
-        System.out.println(rev);
-	}
+        } 
+            return (int) rev;
+ 
+    }
+}
