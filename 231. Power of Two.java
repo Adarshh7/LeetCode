588ceac62a0e292d68b93ede5c974ed32a737e37
Dev Adarsh <devadarsh4900@gmail.com>
2021-06-26 21:42:46 +0530

    Update 231. Power of Two.java

--- 231. Power of Two.java
+++ 231. Power of Two.java
@@ -1,9 +1,8 @@
-public class Power_of_Two  {
-
-	public static void main(String[] args) {
-  int n=2147483647,i=0; long x=0;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
 boolean b=false;
-  while(x<n)
+        int i=0; long x=0;
+    while(x<=n)
   {
    x=(long)Math.pow(2, i);
    i++;
@@ -15,7 +14,7 @@ boolean b=false;
   else 
 	  continue;
    } 
-  System.out.println(b);  
-	}
-
+        return b;      
+  
+    }
 }
