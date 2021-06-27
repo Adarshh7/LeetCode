50af0f3bec0b009992a8f7b07bedb8bc908fa20e
Deb Adarsh Sahu <devadarsh4900@gmail.com>
2021-06-27 23:47:03 +0530

    Update 1688. Count of Matches in Tournament.java

--- 1688. Count of Matches in Tournament.java
+++ 1688. Count of Matches in Tournament.java
@@ -1,21 +1,15 @@
-public class Count of Matches in Tournament {
-
-	public static void main(String[] args) {
-		int n=7,m=0;
-		 while(n!=1) 
-		  { 
-			 if(n%2==0)  
-		      {
-		       n=n/2;
-		       m=m+n/2;
-		      }
-		    else
-		       {
-		       m=m+(n-1)/2;
-		       n=((n-1)/2)+1;
-		       }
-		  }
-		 System.out.println(m);
-	}
-
+class Solution {
+    public int numberOfMatches(int n) {
+        int matches = 0;
+        while(n != 1){
+            if(n % 2 == 0){
+                n = n / 2;
+                matches += n;
+            }else{
+                matches += (n-1)/2;
+                n = ((n -1)/ 2) + 1;
+             }
+         }
+        return matches;
+    }
 }
