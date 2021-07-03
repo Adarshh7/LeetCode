d581db90e0ee0a55a3476233436545469f0ce886
Deb Adarsh Sahu <devadarsh4900@gmail.com>
2021-07-03 23:58:21 +0530

    Update 1470. Shuffle the Array.java

--- 1470. Shuffle the Array.java
+++ 1470. Shuffle the Array.java
@@ -1,25 +1,16 @@
-import java.util.*;
-
-public class Shuffle_the_Array{
-
-	public static void main(String[] args) {
-		Scanner z = new Scanner(System.in);
-		 int n=z.nextInt();
-		   int nums[]= {2,5,1,3,4,7};
-		   int r[]=new int[2*n];
-	        int nums1[]=Arrays.copyOfRange(nums, 0, n);
-	        int nums2[] = Arrays.copyOfRange(nums, n,2*n);
-	        int i=0,j=0;
-	    while( i<2*n)
-	    {
-	       r[i]=nums1[j];
-	       r[i+1]=nums2[j];
-	       i=i+2;
-	       j++;       
-	    }
-	    for(int k=0;k<r.length;k++)
-        {
-        	System.out.println(r[k]);
-        }  
-	    }
-	}
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int r[]=new int[n*2];
+        int nums1[] = Arrays.copyOfRange(nums, 0, n);
+        int nums2[] = Arrays.copyOfRange(nums, n, n*2);
+        int i=0,j=0;
+    while(i<n*2) 
+    {
+       r[i]=nums1[j];
+       r[i+1]=nums2[j];
+        i=i+2;
+        j++;
+    }
+    return r;    
+    }
+}
